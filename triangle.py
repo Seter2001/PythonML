bba2c298297948ea61896e6d53638cce17723970
Seter2001 <74210539+Seter2001@users.noreply.github.com>
2023-10-13 14:10:47 +0200

    add triangle.py

--- triangle.py
+++ triangle.py
@@ -1,45 +1,5 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyOqhv+BqII2uSXeJ/1t8IE0",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/Seter2001/PythonML/blob/master/triangle.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": 12,
-      "metadata": {
-        "id": "BiEERgsB0Z4C"
-      },
-      "outputs": [],
-      "source": [
-        "def triangle(a,b,c):\n",
-        "  if a + b > c and a + c > b and b + c > a:\n",
-        "    return True\n",
-        "  else:\n",
-        "    return False"
-      ]
-    }
-  ]
-}
+def triangle(a,b,c):
+  if a + b > c and a + c > b and b + c > a:
+    return True
+  else:
+    return False
